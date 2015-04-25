abbab5470af1285a0ce646ccd06aa4c997c8b29c
Matt Remick <mremick@gmail.com>
2015-04-25 12:26:56 -0700

    Initial Commit

---
+++ Andpersand/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Andpersand
+//
+//  Created by Matt Remick on 4/25/15.
+//  Copyright (c) 2015 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Andpersand/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Andpersand
+//
+//  Created by Matt Remick on 4/25/15.
+//  Copyright (c) 2015 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
